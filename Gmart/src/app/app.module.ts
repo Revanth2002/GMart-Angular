6ac6ffd05fe7572822ea1831aa411b6eeb77bb77
Revanth <ndrevanth09@gmail.com>
2021-05-07 19:12:26 +0530

    Commits of GMart Project

--- Gmart/src/app/app.module.ts
+++ Gmart/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/component/login/login.component';
+import { RegisterComponent } from './auth/component/register/register.component';
+import { ResetComponent } from './auth/component/reset/reset.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShopComponent,
-    HomeComponent
+    HomeComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
